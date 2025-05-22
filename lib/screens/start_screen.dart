8ad2e1c8962a66202b0746648228b80592d21fb0
robert <rebcave8@gmail.com>
2025-05-22 14:03:52 +0100

    made layout for front screen

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -6,16 +6,32 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Shiritori English'),),
-      body: Column(
-        children: [
-          Container(
-            height: 200,
-            color: Colors.blue,
-            child: Center(child: Text("Header")),
-          ),
-          Expanded(child: Center(child: Text("Some text here"))),
-        ],
+      appBar: AppBar(title: Text('Shiritori English')),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+              child:const Column(
+                children: [
+                  Center(child: Text("Shiritori")),
+                  SizedBox(height: 10),
+                  Center(child: Text("Header")),
+                  Center(child: Text("Header")),
+                  Center(child: Text("Header")),
+                  Center(child: Text("Header")),
+                  Center(child: Text("Header")),
+                ],
+              ),
+            ),
+            // Expanded(child: Center(child: Text("Some text here"))),
+          ],
+        ),
       ),
     );
   }
